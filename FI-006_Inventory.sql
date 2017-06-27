b9122b207f93e1a3947e2f16567814f650e12670
Andy K <andy.h.kwok@gmail.com>
2017-06-27 11:49:52 +0200

    Update FI-006_Inventory.sql

--- FI-006_Inventory.sql
+++ FI-006_Inventory.sql
@@ -40,17 +40,19 @@ CREATE OR REPLACE VIEW tableau."FI-006_Inventory" AS
             get_fi006."Inventory_obsolete",
             get_fi006."Inventory_lastmovement"
            FROM report.get_fi006('2015-01-01'::date::timestamp without time zone, 'now'::text::date::timestamp without time zone, 32) get_fi006("Period_date", "Site", "Internal_reference", "Inventory_quantity", "Inventory_location", "Inventory_unitprice", "Inventory_value_gross_CUR", "Inventory_value_gross_EUR", "Inventory_value_net_CUR", "ratePerEur", "Inventory_value_net_EUR", "Inventory_obsolete", "Inventory_lastmovement")
-		  --condition : la valeur de la Inventory_value_gross_EUR - Inventory_value_net_EUR doit être supérieur à 0
+	--condition : la valeur de la Inventory_value_gross_EUR - Inventory_value_net_EUR doit être supérieur à 0
           WHERE (get_fi006."Inventory_value_gross_EUR" - get_fi006."Inventory_value_net_EUR") > 0::numeric
         ), 
---recupère 3 champs de la vue Axis_Reference dans la sous requête axis_reference		
+       --recupère 3 champs de la vue Axis_Reference dans la sous requête axis_reference		
 		axis_reference AS (
          SELECT "Axis_Reference"."Sc_inventory_type",
             "Axis_Reference"."Site",
             "Axis_Reference"."Reference_internal"
            FROM report."Axis_Reference"
-		  --condition : recupère uniquement les enregistrements ou le Site de la Axis_Reference est égal à celle de la première sous requête inventory
-		  --ainsi que la Reference_internal de la Axis_Reference est égal à la Internal_reference de la première sous requête inventory
+	--condition : recupère uniquement les enregistrements ou le Site de la Axis_Reference 
+	--est égal à celle de la première sous requête inventory
+	--ainsi que la Reference_internal de la Axis_Reference est égal à la Internal_reference 
+	--de la première sous requête inventory
           WHERE (("Axis_Reference"."Site"::text, "Axis_Reference"."Reference_internal"::text) IN ( SELECT inventory_1."Site",
                     inventory_1."Internal_reference"
                    FROM inventory inventory_1))
