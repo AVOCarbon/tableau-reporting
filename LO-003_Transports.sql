efc6cb2e006d7d5c9fbab92ea68ac31781d6bd4b
Andy K <andy.h.kwok@gmail.com>
2017-07-05 16:32:53 +0200

    Update LO-003_Transports.sql 29/06/17
    29/06/17:
    -----------
    
    updating all the comments

--- LO-003_Transports.sql
+++ LO-003_Transports.sql
@@ -1,12 +1,16 @@
--- View: tableau."LO-003_Transports"
+--	View: tableau."LO-003_Transports"
 
--- DROP VIEW tableau."LO-003_Transports";
+--	DROP VIEW tableau."LO-003_Transports";
 
---Crée la vue tableau."LO-003_Transports"
+--	Create view tableau."LO-003_Transports"
 CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS 
---recupère les 2 requêtes ci-dessous dans la sous requête Receptions_data
- WITH "Receptions_data" AS (
-		--récupère 18 champs de la fonction get_lo001 dans la sous requête receptions
+--	CTE Receptions_data" : Has one CTE in it called "receptions"
+-- 	This CTE was  inadvertently built and is only used at the end of 
+--	the main CTE, "Transports_data" 
+WITH "Receptions_data" AS 
+(
+--	retrieve in that CTE "Receptions_data", 18 fields using the "get_lo001" function 
+--	in the sub-sub CTE "receptions"
          WITH receptions AS (
                  SELECT get_lo001."Period_date",
                     get_lo001."Site",
@@ -26,9 +30,31 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
                     get_lo001."Purchasing_currency",
                     get_lo001."Movement_date",
                     get_lo001."Price_change"
-                   FROM report.get_lo001('2015-01-01'::date::timestamp without time zone, 'now'::text::date::timestamp without time zone, 32) get_lo001("Period_date", "Site", "Supplier_code", "Internal_reference", "Shipment_number", "PO_number", "Quantity", "Movement_value_EUR", "Movement_value_CUR", "Reception_price_EUR", "Reception_price_CUR", "Variance_refprice_EUR", "Variance_refprice_CUR", "Variance_value@refprice_EUR", "Variance_value@refprice_CUR", "Purchasing_currency", "Movement_date", "Price_change")
+                   FROM report.get_lo001('2015-01-01'::date::timestamp without time zone, 
+					 '2017-04-25'::date::timestamp without time zone, 32) 
+		 get_lo001("Period_date", 
+			   "Site", 
+			   "Supplier_code", 
+			   "Internal_reference", 
+			   "Shipment_number", 
+			   "PO_number", 
+			   "Quantity", 
+			   "Movement_value_EUR", 
+			   "Movement_value_CUR", 
+			   "Reception_price_EUR", 
+			   "Reception_price_CUR", 
+			   "Variance_refprice_EUR", 
+			   "Variance_refprice_CUR", 
+			   "Variance_value@refprice_EUR", 
+			   "Variance_value@refprice_CUR", 
+			   "Purchasing_currency", 
+			   "Movement_date", 
+			   "Price_change")
                 )
-		 --récupère 5 champs de la sous requête receptions afin de crée 2 nouveau champs : la somme des receptions."Movement_value_EUR" et la somme des receptions."Quantity" grouper par Period_date, Site et Supplier_code
+--	retrieve 5 fields from the sub CTE, "receptions" to create 2 new fields : 
+--	"Movement_value_EUR" = sum in Euros in receptions (Example : you received 20 000 Euros of brushes from Kunshan) 
+--	"Quantity" = sum of quantity received (Example : you received 20 000 brushes from Kunshan)
+--	The query is group by Period_date, Site and Supplier_code
          SELECT receptions."Period_date",
             receptions."Site",
             receptions."Supplier_code",
@@ -36,10 +62,12 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
             sum(receptions."Quantity") AS "Reception_qty"
            FROM receptions
           GROUP BY receptions."Period_date", receptions."Site", receptions."Supplier_code"
-        ), 
---récupère 28 champs de la fonction report.get_lo003 dans la sous requête Transports_data
-		"Transports_data" AS (
-         WITH transports AS (
+), 
+"Transports_data" AS 
+(
+--	retrieve 28 rows from the function "report.get_lo003" and store them in the  CTE, "transports"
+         WITH transports AS 
+				(
                  SELECT get_lo003."Period_date",
                     get_lo003."Site",
                     get_lo003."Carrier_code",
@@ -48,13 +76,16 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
                     get_lo003.to_code,
                     get_lo003."Destination",
                     get_lo003."ETD_date",
-						--stock dans ATD_date la ETD_date de la get_lo003 si la ATD_date est null, sinon la ATD_date
+--	logic : if get_lo003."ATD_date" (or attended date) is null then put get_lo003."ETD_date" (or estimated date)
+--	else  get_lo003."ATD_date" (or attended date)
                         CASE
                             WHEN get_lo003."ATD_date" IS NULL THEN get_lo003."ETD_date"::date::timestamp without time zone
                             ELSE get_lo003."ATD_date"::date::timestamp without time zone
-                        END AS "ATD_date",
+                        END 
+							AS "ATD_date",
                     get_lo003."ETA_date",
-						--stock dans la ATA_date la ETA_date de la get_lo003 si la ATA_date est null, sinon la ATA_date
+--	logic : if get_lo003."ATA_date" is null then put get_lo003."ETD_date" (or ETA date)
+--	else  get_lo003."ATA_date" (or ATA date)
                         CASE
                             WHEN get_lo003."ATA_date" IS NULL THEN get_lo003."ETA_date"::date::timestamp without time zone
                             ELSE get_lo003."ATA_date"::date::timestamp without time zone
@@ -80,10 +111,40 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
                             WHEN get_lo003."TA_delay_days" <> 0 THEN 0
                             ELSE 1
                         END AS on_time_prop
-                   FROM report.get_lo003('2015-01-01'::date::timestamp without time zone, 'now'::text::date::timestamp without time zone, 32) get_lo003("Period_date", "Site", "Carrier_code", from_code, "Origin", to_code, "Destination", "ETD_date", "ATD_date", "ETA_date", "ATA_date", "TD_delay_days", "TA_delay_days", "E_transit_days", "A_transit_days", "Shipment_mode", "Shipment_type", "Premium_freight", "Costing_unit", "Units_shipped", "Invoice_amount_CUR", "Invoice_amount_EUR", "Transport_cost_CUR", "Transport_cost_EUR", "Tax_duty_cost_CUR", "Tax_duty_cost_EUR", "Payer_code", "Total_cost_CUR", "Total_cost_EUR")
+                   FROM report.get_lo003('2015-01-01'::date::timestamp without time zone, '2017-04-25'::date::timestamp without time zone, 32) 
+				   get_lo003("Period_date", "Site", "Carrier_code", 
+				   from_code, 
+				   "Origin", 
+				   to_code, 
+				   "Destination", 
+				   "ETD_date", 
+				   "ATD_date", 
+				   "ETA_date", 
+				   "ATA_date", 
+				   "TD_delay_days", 
+				   "TA_delay_days", 
+				   "E_transit_days", 
+				   "A_transit_days", 
+				   "Shipment_mode", 
+				   "Shipment_type", 
+				   "Premium_freight", 
+				   "Costing_unit", 
+				   "Units_shipped", 
+				   "Invoice_amount_CUR", 
+				   "Invoice_amount_EUR", 
+				   "Transport_cost_CUR", 
+				   "Transport_cost_EUR", 
+				   "Tax_duty_cost_CUR", 
+				   "Tax_duty_cost_EUR", 
+				   "Payer_code", 
+				   "Total_cost_CUR", 
+				   "Total_cost_EUR")
                 ), 
---recupère 16 champs de la vue Axis_Customers_Suppliers dans la sous requête customers_suppliers
-				customers_suppliers AS (
+--	retrieve 16 fields from the view "Axis_Customers_Suppliers" and store them in the CTE, "customers_suppliers"
+--	condition/logic : the CTE "customers_suppliers" retrieve rows where the field "Site" and "Customer_code" of "Axis_Customers_Suppliers" 
+-- 	is equal to the fieldd "Site" and "from_code" in the CTE, "transports" 
+		customers_suppliers AS 
+				(
                  SELECT "Axis_Customers_Suppliers"."Site",
                     "Axis_Customers_Suppliers"."Customer_global",
                     "Axis_Customers_Suppliers"."Customer_code",
@@ -101,14 +162,15 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
                     "Axis_Customers_Suppliers"."Selling_payment_term_days",
                     "Axis_Customers_Suppliers"."Selling_payment_term_type"
                    FROM report."Axis_Customers_Suppliers"
-				  --condition : récupère uniquement les enregistrements ou le Site de la Axis_Customers_Suppliers est égal à celle de la sous requête transports 
-				  --ainsi que le Customer_code de la Axis_Customers_Suppliers est égal au from_code de la sous requête transports
                   WHERE (("Axis_Customers_Suppliers"."Site"::text, "Axis_Customers_Suppliers"."Customer_code"::text) IN ( SELECT transports."Site",
                             transports.from_code
                            FROM transports ))
                 ), 
---recupère 16 champs de la vue Axis_Customers_Suppliers dans la sous requête customers_suppliers2		
-				customers_suppliers2 AS (
+--	retrieve 16 fields from "Axis_Customers_Suppliers" and store them in the CTE, "customers_suppliers2"
+--	condition/logic : retrieve records when "Site" and "Customer_code" of "Axis_Customers_Suppliers" is equal  
+--	to "Site" and "to_code" from the CTE, "transports"	
+		customers_suppliers2 AS 
+				(
                  SELECT "Axis_Customers_Suppliers"."Site",
                     "Axis_Customers_Suppliers"."Customer_global",
                     "Axis_Customers_Suppliers"."Customer_code",
@@ -126,14 +188,16 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
                     "Axis_Customers_Suppliers"."Selling_payment_term_days",
                     "Axis_Customers_Suppliers"."Selling_payment_term_type"
                    FROM report."Axis_Customers_Suppliers"
-				  --condition : récupère uniquement les enregistrements ou le Site de la Axis_Customers_Suppliers est égal à celle de la sous requête transports
-				  ----ainsi que le Customer_code de la Axis_Customers_Suppliers est égal au to_code de la sous requête transports
+
                   WHERE (("Axis_Customers_Suppliers"."Site"::text, "Axis_Customers_Suppliers"."Customer_code"::text) IN ( SELECT transports."Site",
                             transports.to_code
                            FROM transports))
                 ), 
---recupère 16 champs de la vue Axis_Customers_Suppliers dans la sous requête customers_suppliers3				
-				customers_suppliers3 AS (
+--	retrieve 16 fields from "Axis_Customers_Suppliers" in the CTE, "customers_suppliers3"
+--	condition/logic : retrieve records when "Site" and "Customer_code" of "Axis_Customers_Suppliers" is equal to
+--	"Site" and "Carrier_code" from the CTE,	"transports"		
+		customers_suppliers3 AS 
+				(
                  SELECT "Axis_Customers_Suppliers"."Site",
                     "Axis_Customers_Suppliers"."Customer_global",
                     "Axis_Customers_Suppliers"."Customer_code",
@@ -151,14 +215,16 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
                     "Axis_Customers_Suppliers"."Selling_payment_term_days",
                     "Axis_Customers_Suppliers"."Selling_payment_term_type"
                    FROM report."Axis_Customers_Suppliers"
-				  --condition : récupère uniquement les enregistrements ou le Site de la Axis_Customers_Suppliers est égal à celle de la sous requête transports 
-				  ----ainsi que le Customer_code de la Axis_Customers_Suppliers est égal au Carrier_code de la sous requête transports
+
                   WHERE (("Axis_Customers_Suppliers"."Site"::text, "Axis_Customers_Suppliers"."Customer_code"::text) IN ( SELECT transports."Site",
                             transports."Carrier_code"
                            FROM transports))
                 ), 
---recupère 22 champs de la table T03_Suppliers dans la sous requête t03_suppliers		
-				t03_suppliers AS (
+--	retrieve 22 field from the table "T03_Suppliers" in the CTE, "t03_suppliers"
+--	condition/logic : retrieve records where "Site" and "Supplier_code" in "T03_Suppliers" is equal to 
+--	"Supplier_code" and "from_code" from the CTE, "transports"		
+		t03_suppliers AS 
+				(
                  SELECT "T03_Suppliers"."Supplier_code",
                     "T03_Suppliers"."Supplier_name",
                     "T03_Suppliers"."Country_ISO3_C05",
@@ -182,30 +248,34 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
                     "T03_Suppliers"."Payment_term_type",
                     "T03_Suppliers"."Supplier_revision_level"
                    FROM dw."T03_Suppliers"
-				  --condition : recupère uniquement les enregistrements ou le Site de la T03_Suppliers est égal à celle de la sous requête transports
-				  --ainsi que le Supplier_code de la T03_Suppliers égal au from_code de la sous requête transports
                   WHERE (("T03_Suppliers"."Site"::text, "T03_Suppliers"."Supplier_code"::text) IN ( SELECT transports."Site",
                             transports.from_code
                            FROM transports))
                 ), 
---recupère 5 champs de la sous requête Receptions_data dans receptions_data			
-				receptions_data AS (
+--	retrieve 5 fields from the CTE above, "Receptions_data" and store them in the CTE "receptions_data" below (beware of the 'case sensitive naming' 
+--	as "receptions_data" != "Receptions_data")	
+--	condition/logic : retrieve rows where "Site" , "Supplier_code" and "Period_date" from CTE, "Receptions_data" is equal "to_code" , "from_code"  
+--	"Period_date" in CTE, "transports"		
+		receptions_data AS 
+				(
                  SELECT "Receptions_data"."Period_date",
                     "Receptions_data"."Site",
                     "Receptions_data"."Supplier_code",
                     "Receptions_data"."Reception_value_EUR",
                     "Receptions_data"."Reception_qty"
                    FROM "Receptions_data"
-				  --condition : recupère uniquement les enregistrements ou le Site de la sous requête Receptions_data est égal au to_code de la sous requête receptions 
-				  --ainsi que le Supplier_code de la sous requête Receptions_data est égal au from_code de la sous requête receptions 
-				  --ainsi que la Period_date de la sous requête Receptions_data est égal à celui de la sous requête receptions
+
                   WHERE (("Receptions_data"."Site"::text, "Receptions_data"."Supplier_code"::text, "Receptions_data"."Period_date") IN ( SELECT transports.to_code,
                             transports.from_code,
                             transports."Period_date"
                            FROM transports))
                 )
---recupère 5 champs de la sous requête customers_suppliers, 6 champs de la sous requête t03_suppliers, 4 champs de la sous requête customers_suppliers2, 1 champ de la sous requête customers_suppliers3
---2 champs de la sous requête receptions_data et 28 champs de la sous requête transports
+--	retrieve 5 fields from the CTE, "customers_suppliers"
+--  6 fields from the CTE, "t03_suppliers", 
+--	4 fields from the CTE, "customers_suppliers2" 
+--	1 field from the CTE, "customers_suppliers3"
+--	2 field from the CTE, "receptions_data" 
+--	28 fields from the CTE, "transports"
          SELECT customers_suppliers."Customer_name" AS "Supplier_name",
             customers_suppliers."Customer_incoterm"::text AS "Supplier_incoterm",
             customers_suppliers."Customer_interco" AS "Supplier_interco",
@@ -252,14 +322,19 @@ CREATE OR REPLACE VIEW tableau."LO-003_Transports" AS
             transports."Total_cost_EUR",
             transports.on_time_prop
            FROM transports
-		     --jointure des sous requêtes
-             LEFT JOIN report."Axis_Customers_Suppliers" customers_suppliers ON transports."Site"::text = customers_suppliers."Site"::text AND transports.from_code::text = customers_suppliers."Customer_code"::text
-             LEFT JOIN dw."T03_Suppliers" t03_suppliers ON transports."Site"::text = t03_suppliers."Site"::text AND transports.from_code::text = t03_suppliers."Supplier_code"::text
-             LEFT JOIN report."Axis_Customers_Suppliers" customers_suppliers2 ON transports."Site"::text = customers_suppliers2."Site"::text AND transports.to_code::text = customers_suppliers2."Customer_code"::text
-             LEFT JOIN report."Axis_Customers_Suppliers" customers_suppliers3 ON transports."Site"::text = customers_suppliers3."Site"::text AND transports."Carrier_code"::text = customers_suppliers3."Customer_code"::text
-             LEFT JOIN "Receptions_data" receptions_data ON transports.to_code::text = receptions_data."Site"::text AND transports.from_code::text = receptions_data."Supplier_code"::text AND transports."Period_date" = receptions_data."Period_date"
-        )
---recupère 48 champs de la sous requête Transports_data 
+--	join CTEs and functions or views or tables
+             LEFT JOIN report."Axis_Customers_Suppliers" customers_suppliers ON transports."Site"::text = customers_suppliers."Site"::text 
+			 AND transports.from_code::text = customers_suppliers."Customer_code"::text
+             LEFT JOIN dw."T03_Suppliers" t03_suppliers ON transports."Site"::text = t03_suppliers."Site"::text 
+			 AND transports.from_code::text = t03_suppliers."Supplier_code"::text
+             LEFT JOIN report."Axis_Customers_Suppliers" customers_suppliers2 ON transports."Site"::text = customers_suppliers2."Site"::text 
+			 AND transports.to_code::text = customers_suppliers2."Customer_code"::text
+             LEFT JOIN report."Axis_Customers_Suppliers" customers_suppliers3 ON transports."Site"::text = customers_suppliers3."Site"::text 
+			 AND transports."Carrier_code"::text = customers_suppliers3."Customer_code"::text
+             LEFT JOIN "Receptions_data" receptions_data ON transports.to_code::text = receptions_data."Site"::text 
+			 AND transports.from_code::text = receptions_data."Supplier_code"::text AND transports."Period_date" = receptions_data."Period_date"
+)
+--	retrieve 48 fields from the CTE, "Transports_data" 
  SELECT "Transports_data"."Supplier_name",
     "Transports_data"."Supplier_incoterm",
     "Transports_data"."Supplier_interco",
